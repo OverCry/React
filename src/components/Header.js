b4d747782e49be4a343a017d540cb5e7b9813f08
Wong Chong <wcho400@aucklanduni.ac.nz>
2021-02-04 22:48:38 +1300

    starting mock backend

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header = ({title, onAdd, showAdd}) => {
     return (
         <header className='header'>
             <h1 >{title}</h1>
-            <Button color='green' text={showAdd ? ' Hide' : 'Add'} onClick={onAdd} />
+            <Button color={showAdd ? 'red' : 'green'} text={showAdd ? ' Hide' : 'Add'} onClick={onAdd} />
         </header>
     )
 }
