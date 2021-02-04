e6e35e4225795e150463db02c19813d869bf52e4
Wong Chong <wcho400@aucklanduni.ac.nz>
2021-02-04 14:33:32 +1300

    create component folder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  //always returns a single parent element
+  // basically, encapsulate everything in one <>
+  // if you dont want it to be grouped as a div, use an empty angle bracket
+  const name = 'Wong'
+
+  //can write javascript right into {}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Hello From React</h1>
+      <h2>Hello {name}</h2>
     </div>
   );
 }
