18671b563b1e233adeb7e4776fce621c1d20da2e
Wong Chong <wcho400@aucklanduni.ac.nz>
2021-02-05 18:10:37 +1300

    add location check

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,15 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
+import {useLocation} from 'react-router-dom' //to see what path it is on
 
 
 const Header = ({title, onAdd, showAdd}) => {
+    const location = useLocation()
 
     return (
         <header className='header'>
             <h1 >{title}</h1>
-            <Button color={showAdd ? 'red' : 'green'} text={showAdd ? ' Hide' : 'Add'} onClick={onAdd} />
+            {location.pathname==='/' && <Button color={showAdd ? 'red' : 'green'} text={showAdd ? ' Hide' : 'Add'} onClick={onAdd} /> }
         </header>
     )
 }
